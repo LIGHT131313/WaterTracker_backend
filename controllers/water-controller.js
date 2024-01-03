7ce456906f82613a98b4b8aa777b0b740c1ca8df
Fetivol <Fetisowova@gmail.com>
2024-01-03 16:51:13 +0100

    Update water-controller.js

--- controllers/water-controller.js
+++ controllers/water-controller.js
@@ -27,39 +27,6 @@ const addWaterValue = async (req, res) => {
 
   res.status(201).json(result);
 };
-// const updateWaterValueByID = async (req, res) => {
-//   const { id } = req.params;
-//   const { _id: owner } = req.user;
-//   const { waterVolume, date } = req.body;
-
-//   const existingRecord = await WaterValue.findOne({ _id: id, owner });
-//   if (!existingRecord) {
-//     throw HttpError(404, "WaterVolume not found");
-//   }
-
-//   if (date && date !== existingRecord.date) {
-//     const dateConflict = await WaterValue.findOne({
-//       date,
-//       owner,
-//       _id: { $ne: id },
-//     });
-//     if (dateConflict) {
-//       throw HttpError(
-//         409,
-//         `WaterVolume with this date ${date} already exists in DB`
-//       );
-//     }
-//   }
-
-//   const result = await WaterValue.findOneAndUpdate(
-//     { _id: id, owner },
-//     { waterVolume, date },
-//     { new: true }
-//   );
-
-//   res.json(result);
-// };
-
 const updateWaterValueByID = async (req, res) => {
   const { id } = req.params;
   const { _id: owner } = req.user;
@@ -84,38 +51,13 @@ const updateWaterValueByID = async (req, res) => {
     }
   }
 
-  if (waterVolume !== existingRecord.waterVolume) {
-    const volumeConflict = await WaterValue.findOne({
-      date: existingRecord.date,
-      owner,
-      waterVolume,
-      _id: { $ne: id },
-    });
-    if (volumeConflict) {
-      throw HttpError(
-        409,
-        `WaterVolume with this date ${date} and volume ${waterVolume} already exists in DB`
-      );
-    }
-  }
+  const result = await WaterValue.findOneAndUpdate(
+    { _id: id, owner },
+    { waterVolume, date },
+    { new: true }
+  );
 
-  if (date === existingRecord.date) {
-    const result = await WaterValue.findOneAndUpdate(
-      { _id: id, owner },
-      { waterVolume },
-      { new: true }
-    );
-
-    res.json(result);
-  } else {
-    const result = await WaterValue.findOneAndUpdate(
-      { _id: id, owner },
-      { waterVolume, date },
-      { new: true }
-    );
-
-    res.json(result);
-  }
+  res.json(result);
 };
 
 const deleteteWaterValueByID = async (req, res) => {
