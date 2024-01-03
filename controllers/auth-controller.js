590c8dc3b4a44557d0bab5b59eadab9828ec7cc4
Fasten-belts <124335932+Fasten-belts@users.noreply.github.com>
2024-01-03 12:19:08 +0100

    base url

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -163,7 +163,9 @@ const requestPasswordReset = async (req, res) => {
     html: ` <div style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; margin: 0; padding: 20px; background-color: #f4f7fa;">
       <div style="max-width: 600px; margin: 20px auto; padding: 20px; background-color: #ffffff; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
        <div style="text-align: center;">
+        <a href="${BASE_URL}">
           <img src="https://yanlozovskyi.github.io/water-tracker/assets/Logo-890d13ba.png" alt="Logo" style="max-width: 100px; margin: 0 auto;">
+          </a>
         </div>
         <h2 style="color: #407BFF; font-size: 24px; margin-bottom: 20px;">Password Reset Request</h2>
         <p style="color: #333333; font-size: 16px; line-height: 1.5;">Hello,</p>
@@ -175,7 +177,9 @@ const requestPasswordReset = async (req, res) => {
         <p style="color: #333333; font-size: 16px; line-height: 1.5;">If you are having trouble clicking the link, please copy and paste it into your web browser's address bar.</p>
         <p style="color: #333333; font-size: 16px; margin-top: 30px;">Thank you, ${userName}</p>
         <div style="text-align: center;">
+        <a href="${BASE_URL}">
           <img src="https://yanlozovskyi.github.io/water-tracker/assets/Logo-890d13ba.png" alt="Logo" style="max-width: 100px; margin: 0 auto;">
+          </a>
         </div>
       </div>
     </div>`,
